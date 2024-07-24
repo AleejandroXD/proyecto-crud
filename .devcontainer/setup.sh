11084141370e86b5da99ec6268031bd2d8656cab
Carlos Samaniego <carlos.samaniego@hogarth.com>
2024-07-24 12:42:06 -0300

    Setup fixes

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp env.example .env
 composer install
 npm install --legacy-peer-deps
 php artisan migrate:fresh --seed --force
--- app/Models/Nota.php
+++ app/Models/Nota.php
@@ -13,30 +13,55 @@ class Nota extends Model
 
     public function estudiantes()
     {
-        return $this->belongsTo(Estudiantes::class, "estudiante_id");// La clase Nota tiene una relación de muchos a 1 con Estudiantes, porque cada nota pertenece a un estudiante.
+        // La clase Nota tiene una relación de muchos a 1 con Estudiantes, porque cada nota pertenece a un estudiante.
+        return $this->belongsTo(Estudiantes::class, "estudiante_id");
     }
 
     public function materias()
     {
-        return $this->belongsTo(Materia::class, "materias_id");// La clase Nota tiene una relación de muchos a 1 con Materias, porque cada nota pertenece a una materia.
+        // La clase Nota tiene una relación de muchos a 1 con Materias, porque cada nota pertenece a una materia.
+        return $this->belongsTo(Materia::class, "materias_id");
     }
     
-    protected function nombreEstudiante(): Attribute //Definimos la funcion como un accesor que modifica los nombre de los valores del atributo.
+    /**
+     * Definimos la funcion como un accesor que modifica los nombre de los valores del
+     * atributo.
+     * 
+     */
     {
+    protected function nombreEstudiante(): Attribute
+    { 
         return Attribute::make( //Cuando se ejecuta la funcion 
             get: fn ($value) => $this->estudiantes->nombres,//accedemos al valor nombre de estudiantes
         );  
     }
+
+    /**
+     *
+     */
     protected function nombreMateria(): Attribute//Definimos la funcion como un accesor que modifica los nombre de los valores del atributo.
     {
         return Attribute::make(//Cuando se ejecuta la funcion 
             get: fn ($value) => $this->materias->nombre,//accedemos al valor nombre de materias
         );  
     }
-    public static function existeNota(int $estudiante_id, int $materias_id){ //Agregamos los parametros con el tipo de dato //le damos la propiedad static para no instanciar a la funcion
-        $existeNota = DB::table('nota')->where('estudiante_id', $estudiante_id)//Busca si existe el estudiante
-                          ->where('materias_id', $materias_id)//Busca si existe la materia 
-                          ->exists();//Si las dos cosas existen al mismo tiempo entonces $existeNota es true
+
+    /**
+     * Agregamos los parametros con el tipo de dato
+     * le damos la propiedad static para no instanciar a la funcion.
+     * 
+     * @param int $estudiante_id ID del estudiante
+     * @param int $materias_id ID de la materia
+     * 
+     * @return bool
+     */
+    public static function existeNota(int $estudiante_id, int $materias_id): bool
+    {
+        //Busca si existe el estudiante
+        $existeNota = DB::table('nota')->where('estudiante_id', $estudiante_id)
+              ->where('materias_id', $materias_id)//Busca si existe la materia 
+              ->exists();//Si las dos cosas existen al mismo tiempo entonces $existeNota es true
+
         return $existeNota;
     }
 }
