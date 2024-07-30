44e5f0156c1d9daf130a24c56a57d3ff5176a51f
Carlos Samaniego <carlos.samaniego@hogarthww.com>
2024-07-30 11:39:48 -0300

    Fix routes permissions & fix depdencies

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/form', [MateriaController::class, 'showForm']);
-Route::match(['get', 'post'],'/estudiantes', [App\Http\Controllers\EstudiantesController::class, 'index'])->name('estudiantes.index');
-Route::match(['get', 'post'],'/notas', [\App\Http\Controllers\NotasController::class, 'index'])->name('notas.index');
+
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/form', [MateriaController::class, 'showForm']);
+    Route::match(['get', 'post'],'/estudiantes', [App\Http\Controllers\EstudiantesController::class, 'index'])->name('estudiantes.index');
+    Route::match(['get', 'post'],'/notas', [\App\Http\Controllers\NotasController::class, 'index'])->name('notas.index');
 });
 
 require __DIR__.'/auth.php';
