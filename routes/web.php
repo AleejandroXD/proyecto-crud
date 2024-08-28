b8c944b8788f3509595a463fc1ed706e17680660
Carlos Samaniego <carlos.samaniego@hogarth.com>
2024-08-28 11:23:59 -0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,11 +24,11 @@ Route::middleware('auth')->group(function () {
         return view('welcome');
     });
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/form', [MateriaController::class, 'showForm']);
+    Route::get('/form', [App\Http\Controllers\MateriaController::class, 'showForm']);
     Route::match(['get', 'post'],'/estudiantes', [App\Http\Controllers\EstudiantesController::class, 'index'])->name('estudiantes.index');
     Route::match(['get', 'post'],'/notas', [\App\Http\Controllers\NotasController::class, 'index'])->name('notas.index');
 });
